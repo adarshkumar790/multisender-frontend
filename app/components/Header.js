7bb8275c635dfa06c82c6719453169ff80f1f3a0
AdarhKumar <adarshnit420@gmail.com>
2024-12-03 11:56:49 +0530

    first commit

--- app/components/Header.js
+++ app/components/Header.js
@@ -8,24 +8,25 @@ import WalletConnectProvider from "@walletconnect/web3-provider";
 
 export default function Header() {
   const [walletAddress, setWalletAddress] = useState("");
-  const [ethBalance, setEthBalance] = useState(0); 
+  const [ethBalance, setEthBalance] = useState(0);
   const [showWalletOptions, setShowWalletOptions] = useState(false);
   const [showNetworkOptions, setShowNetworkOptions] = useState(false);
   const [showVipModal, setShowVipModal] = useState(false);
   const [selectedNetwork, setSelectedNetwork] = useState("Ethereum Mainnet");
   const [provider, setProvider] = useState(null);
 
-  
-  const networks = ["Ethereum Mainnet", "BNB Smart Chain", "Polygon (MATIC)"];
+  const networks = [
+    { name: "Ethereum Mainnet", chainId: "0x1" },
+    { name: "BNB Smart Chain", chainId: "0x38" },
+    { name: "Polygon (MATIC)", chainId: "0x89" },
+  ];
 
   const vipTiers = [
     { id: "starter", name: "Starter - 1 day", price: 0.4 },
     { id: "professional", name: "Professional - 7 days", price: 0.8 },
     { id: "business", name: "Business - 1 month", price: 1.2 },
-    // { id: "enterprise", name: "Enterprise - Custom", price: Infinity },
   ];
 
-  // Connect to MetaMask
   const connectMetaMask = async () => {
     if (typeof window.ethereum !== "undefined") {
       try {
@@ -35,12 +36,9 @@ export default function Header() {
         const balance = await web3.eth.getBalance(accounts[0]);
         const userAddress = accounts[0];
 
-        console.log("MetaMask connected:", userAddress);
-        console.log("Balance in ETH:", web3.utils.fromWei(balance, "ether"));
-
         setWalletAddress(userAddress);
         setEthBalance(Number(web3.utils.fromWei(balance, "ether")));
-        setShowWalletOptions(false); 
+        setShowWalletOptions(false);
       } catch (error) {
         console.error("Error connecting to MetaMask:", error.message);
         alert("Error connecting to MetaMask. Please try again.");
@@ -50,7 +48,6 @@ export default function Header() {
     }
   };
 
-  
   const connectWalletConnect = async () => {
     try {
       const walletConnectProvider = new WalletConnectProvider({
@@ -66,13 +63,10 @@ export default function Header() {
       const balance = await web3.eth.getBalance(accounts[0]);
       const userAddress = accounts[0];
 
-      console.log("WalletConnect connected:", userAddress);
-      console.log("Balance in ETH:", web3.utils.fromWei(balance, "ether"));
-
       setWalletAddress(userAddress);
       setEthBalance(Number(web3.utils.fromWei(balance, "ether")));
       setProvider(walletConnectProvider);
-      setShowWalletOptions(false); 
+      setShowWalletOptions(false);
     } catch (error) {
       console.error("Error connecting to WalletConnect:", error.message);
       alert("Error connecting to WalletConnect. Please try again.");
@@ -90,10 +84,27 @@ export default function Header() {
 
   const isTierActive = (price) => ethBalance >= price;
 
-  
-  const changeNetwork = (network) => {
-    setSelectedNetwork(network);
-    setShowNetworkOptions(false); 
+  const changeNetwork = async (network) => {
+    try {
+      const { chainId } = network;
+      if (window.ethereum) {
+        await window.ethereum.request({
+          method: "wallet_switchEthereumChain",
+          params: [{ chainId }],
+        });
+        setSelectedNetwork(network.name);
+        setShowNetworkOptions(false);
+      } else {
+        alert("MetaMask is not installed. Please install it to change networks.");
+      }
+    } catch (error) {
+      console.error("Error switching networks:", error.message);
+      if (error.code === 4902) {
+        alert("Network not added to MetaMask. Please add it manually.");
+      } else {
+        alert("Error switching networks. Please try again.");
+      }
+    }
   };
 
   return (
@@ -186,11 +197,11 @@ export default function Header() {
             <div className="space-y-2">
               {networks.map((network) => (
                 <button
-                  key={network}
+                  key={network.chainId}
                   onClick={() => changeNetwork(network)}
                   className="block w-full px-4 py-2 bg-gray-600 text-blue-300 rounded-lg mb-2 hover:text-gray-100 hover:bg-gray-400"
                 >
-                  {network}
+                  {network.name}
                 </button>
               ))}
             </div>
@@ -201,61 +212,56 @@ export default function Header() {
       {/* VIP Modal */}
       {showVipModal && (
         <div className="fixed inset-0 bg-[#0F123D] bg-opacity-50 flex justify-center items-center z-50">
-          <div className="bg-gray-700 rounded-lg p-6 w-96 relative flex">
+          <div className="bg-gray-700 rounded-lg p-6 w-96 relative">
             <button
               onClick={() => setShowVipModal(false)}
               className="absolute top-2 right-2 text-white text-xl hover:text-gray-100"
             >
               &times;
             </button>
-            <div className="w-full pr-4">
-              <h3 className="text-lg text-blue-500 font-bold mb-4">VIP</h3>
-              <p className="text-slate-400 text-sm mb-4">
-                VIP gives you discounted access to Ledgerline Multisender.app, and all your
-                transactions will be free.
-              </p>
-              <div className="space-y-3">
-                {vipTiers.map((tier) => (
-                  <div
-                    key={tier.id}
-                    className={`flex items-center gap-2 border rounded-lg p-2 w-full ${
-                      isTierActive(tier.price)
-                        ? "border-green-500 bg-green-800"
-                        : "border-gray-500 bg-gray-700"
+            <h3 className="text-lg text-blue-500 font-bold mb-4">VIP</h3>
+            <p className="text-slate-400 text-sm mb-4">
+              VIP gives you discounted access to Ledgerline Multisender.app, and all your
+              transactions will be free.
+            </p>
+            <div className="space-y-3">
+              {vipTiers.map((tier) => (
+                <div
+                  key={tier.id}
+                  className={`flex items-center gap-2 border rounded-lg p-2 ${
+                    isTierActive(tier.price)
+                      ? "border-green-500 bg-green-800"
+                      : "border-gray-500 bg-gray-700"
+                  }`}
+                >
+                  <input
+                    type="radio"
+                    id={tier.id}
+                    name="vip"
+                    disabled={!isTierActive(tier.price)}
+                  />
+                  <label
+                    htmlFor={tier.id}
+                    className={`text-sm cursor-pointer ${
+                      isTierActive(tier.price) ? "text-blue-400" : "text-gray-400"
                     }`}
                   >
-                    <input
-                      type="radio"
-                      id={tier.id}
-                      name="vip"
-                      disabled={!isTierActive(tier.price)}
-                    />
-                    <label
-                      htmlFor={tier.id}
-                      className={`text-sm cursor-pointer ${
-                        isTierActive(tier.price)
-                          ? "text-blue-400"
-                          : "text-blue-400"
-                      }`}
-                    >
-                      {tier.name} - {tier.price} ETH
-                    </label>
-                  </div>
-                  
-                ))}
-              </div>
-              <div className="mt-2 border border-blue-400 py-1 rounded text-blue-200 flex items-center gap-2">
-              <FaTelegram className="ml-2 text-blue-400 size-6" />
-              Enterprise - Custom
-            </div>  
-            <button
-                className="bg-blue-500 w-full mt-3 text-white py-2 rounded"
-                disabled={!walletAddress}
-              >
-                {walletAddress ? "Buy" : "Connect Wallet First"}
-              </button>
+                    {tier.name} - {tier.price} ETH
+                  </label>
+                  {!isTierActive(tier.price) && (
+                    <span className="text-red-500 text-xs ml-auto">
+                      Insufficient Balance
+                    </span>
+                  )}
+                </div>
+              ))}
             </div>
-            
+            <button
+              className="bg-blue-500 w-full mt-3 text-white py-2 rounded"
+              disabled={!walletAddress}
+            >
+              {walletAddress ? "Buy" : "Connect Wallet First"}
+            </button>
           </div>
         </div>
       )}
